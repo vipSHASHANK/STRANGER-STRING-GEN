5132c8ec3de762667121b0ae8d92127e6bfe601e
SHASHANK <primiumshukla474@gmail.com>
2025-07-31 10:27:44 +0530

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -1,8 +1,16 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from config import *
+from config import START_IMG, OWNER_ID
 
-START_TXT = """✦ » ʜᴇʏ  {msg.from_user.mention}  ✤,
+# Custom Filters
+def filter_cmd(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter_cmd("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+
+    START_TXT = f"""✦ » ʜᴇʏ  {msg.from_user.mention}  ✤,
 ✦ » ɪ ᴀᴍ {me2},
 
 ✦ » Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
@@ -11,15 +19,23 @@ START_TXT = """✦ » ʜᴇʏ  {msg.from_user.mention}  ✤,
 
 ✦ » ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ !"""
 
-START_BTN =             [InlineKeyboardButton("ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ", callback_data="generate")],
-                [
-                    InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/MASTIWITHFRIENDSXD"),
-                    InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://tg://user?id={OWNER_ID}")
-                ],
-                [InlineKeyboardButton("📘 ɢᴜɪᴅᴇ", cack_data="guide")]
-            ]
+    START_BTN = [
+        [InlineKeyboardButton("ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ", callback_data="generate")],
+        [
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/MASTIWITHFRIENDSXD"),
+            InlineKeyboardButton("ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER_ID}" if str(OWNER_ID).isnumeric() else f"https://t.me/{OWNER_ID.lstrip('@')}"),
+        ],
+        [InlineKeyboardButton("📘 ɢᴜɪᴅᴇ", callback_data="guide")]
+    ]
 
-GUIDE_TXT ="""✦ ʙᴀsɪᴄ ᴄᴏᴍᴍᴀɴᴅs
+    await bot.send_photo(
+        chat_id=msg.chat.id,
+        photo=START_IMG,
+        caption=START_TXT,
+        reply_markup=InlineKeyboardMarkup(START_BTN),
+    )
+
+GUIDE_TXT = """✦ ʙᴀsɪᴄ ᴄᴏᴍᴍᴀɴᴅs
 
 ➻ ᴛʏᴘᴇ /gen ᴏʀ ᴛᴀᴘ ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ ғᴏʀ ɢᴇɴ sᴇssɪᴏɴ.
 
@@ -29,20 +45,6 @@ GUIDE_TXT ="""✦ ʙᴀsɪᴄ ᴄᴏᴍᴍᴀɴᴅs
 ➻ ᴛʏᴘᴇ /broadcast ᴛᴏ sᴇɴᴅ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴀʟʟ ᴜsᴇʀs + ᴄʜᴀᴛs (ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ)
 
 ⦿ ᴊᴏɪɴ sᴜᴘᴘᴏʀᴛ ғᴏʀ ᴍᴏʀᴇ ᴜᴘᴅᴀᴛᴇs."""
-
-
-def filter(cmd: str):
-    return filters.private & filters.incoming & filters.command(cmd)
-
-@Client.on_message(filter("start"))
-async def start(bot: Client, msg: Message):
-    me2 = (await bot.get_me()).mention
-    await bot.send_photo(
-        chat_id=msg.chat.id,
-        photo=START_IMG,
-        caption=START_TXT,
-        reply_markup=InlineKeyboardMarkup(START_BTN),
-    )
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
@@ -58,6 +60,25 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif data == "start_menu":
         me2 = (await client.get_me()).mention
+
+        START_TXT = f"""✦ » ʜᴇʏ  {query.from_user.mention}  ✤,
+✦ » ɪ ᴀᴍ {me2},
+
+✦ » Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+
+✦ » ᴘʟᴇᴀꜱᴇ ᴄʜᴏᴏꜱᴇ ᴛʜᴇ ᴘʏᴛʜᴏɴ ʟɪʙʀᴀʀʏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ꜰᴏʀ.
+
+✦ » ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ !"""
+
+        START_BTN = [
+            [InlineKeyboardButton("ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ", callback_data="generate")],
+            [
+                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/MASTIWITHFRIENDSXD"),
+                InlineKeyboardButton("ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER_ID}" if str(OWNER_ID).isnumeric() else f"https://t.me/{OWNER_ID.lstrip('@')}"),
+            ],
+            [InlineKeyboardButton("📘 ɢᴜɪᴅᴇ", callback_data="guide")]
+        ]
+
         await query.message.edit_caption(
             caption=START_TXT,
             reply_markup=InlineKeyboardMarkup(START_BTN)
